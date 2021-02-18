535ef8e23e865049c9def5db90fafdaa7dd99ef7
A1exander-Pro <76167473+A1exander-Pro@users.noreply.github.com>
2021-02-18 13:44:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ import pandas
 import collections
 
 
+env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+template = env.get_template('template.html')
+
+
 def get_year_string():
     years_with_you = datetime.datetime.now().year - 1920
     remainder = years_with_you % 10
@@ -17,49 +24,40 @@ def get_year_string():
         return f'Уже {years_with_you} лет с вами'
 
 
-def create_parser():
+def parse_file_name():
     parser = argparse.ArgumentParser(
         description='Использует выбранный файл с номенклатурой винодельни'
     )
     parser.add_argument('file_name', help='файл', type=str)
-    return parser
+    args = parser.parse_args()
+    file_name = args.file_name
+    return file_name
 
 
-def get_assortment(file_name):
-    alcohol_nomenclature = pandas.read_excel(file_name, sheet_name='Лист1',
+def get_assortment():
+    alcohol_nomenclature = pandas.read_excel(parse_file_name(), sheet_name='Лист1',
                                              usecols=['Категория', 'Название', 'Сорт',
                                                       'Цена', 'Картинка', 'Акция'],
                                              na_values=['N/A', 'NA'], keep_default_na=False)
 
-    alcohol_assortment = alcohol_nomenclature.to_dict(orient='records')
+    sorted_nomenclature = alcohol_nomenclature.sort_values(by=['Категория'])
+    alcohol_assortment = sorted_nomenclature.to_dict(orient='records')
     assortment = collections.defaultdict(list)
 
     for bottle in alcohol_assortment:
         category = bottle['Категория']
         assortment[category].append(bottle)
-    sorted_assortment = dict(sorted(assortment.items()))
-    return sorted_assortment
+    return assortment
 
 
-def get_render_page(file_name, template):
-    rendered_page = template.render(
-        years_with_you=get_year_string(),
-        assortment=get_assortment(file_name)
-    )
-    return rendered_page
-
+rendered_page = template.render(
+    years_with_you=get_year_string(),
+    assortment=get_assortment()
+)
 
 if __name__ == '__main__':
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-    template = env.get_template('template.html')
-    parser = create_parser()
-    args = parser.parse_args()
-    file_name = args.file_name
     with open('index.html', 'w', encoding="utf8") as file:
-        file.write(get_render_page(file_name, template))
+        file.write(rendered_page)
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
 
